7ee7b0a47e6827c0f501572155c24c5035805a82
manii1-sh <your-email@example.com>
2026-06-14 22:50:51 +0530

    Add Netlify deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   resolve: {
@@ -13,5 +14,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    netlify(),
   ],
 });
